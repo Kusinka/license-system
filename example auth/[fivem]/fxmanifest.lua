087272b3e5380733546782d8ff3154e207f24478
Kusinka <83094308+Kusinka@users.noreply.github.com>
2022-07-12 12:04:30 +0200

    Add files via upload

---
+++ example auth/[fivem]/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'kusinka#7822'
+description 'license system'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
